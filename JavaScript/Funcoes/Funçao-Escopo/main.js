31a1beb2fb17a1c9bf0b229d18f9ff932f30bdf9
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-16 23:05:04 +0100

    Escopo dentro da funçao

---
+++ JavaScript/Funcoes/Funçao-Escopo/main.js
@@ -0,0 +1,9 @@
+
+
+function foltaVisitar(paisesVisitados){
+    let totalPaises = 193
+    return `Folta visitar ${totalPaises - paisesVisitados}`
+}
+console.log(foltaVisitar(20))
+
+console.log(totalPaises)//Uma veis que declaro minha variavel dentro do escopo da funçao nao consigo açessar ela fora do escopo
