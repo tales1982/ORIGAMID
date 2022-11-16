25c76247ecd57c641dcacc4feefb55aa7a3dcfeb
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-16 22:44:28 +0100

    Funcoes usando IF e Else

--- JavaScript/Funcoes/Funcoes-if-else/main.js
+++ JavaScript/Funcoes/Funcoes-if-else/main.js
@@ -11,3 +11,18 @@ function corFavorita(cor){
 console.log(corFavorita("azul"));
 console.log(corFavorita("verde"));
 console.log(corFavorita(""));
+
+//############################################################################
+//Uma função pode retorna qualquer tipo de dado ate mesmo outra função.
+
+function terceiraIdade(idade){
+   
+    if(typeof idade !== 'number'){
+        return 'Informe sua idade.'
+    }else if(idade >= 60){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(terceiraIdade(60))
