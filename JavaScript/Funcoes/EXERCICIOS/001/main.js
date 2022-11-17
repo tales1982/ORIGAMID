727af7b607ad074f32dc93e280900eac8836710b
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-17 23:05:13 +0100

    Sol ou Chuva

--- JavaScript/Funcoes/EXERCICIOS/001/main.js
+++ JavaScript/Funcoes/EXERCICIOS/001/main.js
@@ -1,13 +1,21 @@
 //Crie uma função para verificar se o valor e true
-const sol = false
+//;
+const sol = prompt("Como esta o dia de hoje\nDigite = sol\nDigite = chuva");
+const section = document.querySelector("#section");
+const res = document.getElementById("res");
 
-function irPraia(){
-    if (sol === true){
-        let  res = document.getElementById("res")
-        res.innerText = "O dia estar muito bonito Vamos para a Praia.";
-        
-    }else{
-        res.innerText = "O dia esta muito chuvoso vamos ficar em casa."
+function irPraia() {
+  
+    if (sol === "sol") {
+      const praiaSol = document.querySelector(".praia-sol");
+      praiaSol.classList.remove("praia-sol");
+      res.innerText = "O dia estar muito bonito Vamos para a Praia.";
+    } else if (sol === "chuva") {
+      const Chuva = document.querySelector(".chuva");
+      Chuva.classList.remove("chuva");
+      res.innerText = "Esta Chovendo muito, vamos deixar para ir outro dia..";
+    } else{
+        alert("Opção invalida.")
     }
-}
+  }
 irPraia()
