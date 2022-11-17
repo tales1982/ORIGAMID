99f6bc93f732a82eda2bef32fdbd469d56ff95c9
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-17 21:06:48 +0100

    Funçao que retorna o nome

---
+++ JavaScript/Funcoes/EXERCICIOS/003/main.js
@@ -0,0 +1,13 @@
+const inputEnviar = document.getElementById("enviar");
+const nome = document.getElementById("nome")
+const sobrenome = document.getElementById("sobrenome")
+const res = document.getElementById("res")
+
+inputEnviar.addEventListener("click", function(ev){
+    ev.preventDefault()
+    res.innerText = ` ${nome.value} ${sobrenome.value}`;
+})
+
+
+
+
